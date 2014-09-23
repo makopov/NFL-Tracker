4fa91929a39042add1d559d0cbb47d78fdc8ba75
Michael Akopov <makopov@gmail.com>
2014-09-22 22:35:47 -0600

    First release commit, had to recreate master to do some issues. All commits leading up to this one are in the develop branch.

---
+++ runner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+device='/dev/vda1'
+script='harvester.py'
+email_address='makopov@gmail.com'
+
+#Run forever
+while true
+do
+	#Check if script is running
+	processCount=`ps -x | grep $script | wc -l`
+	echo $processCount
+	if [ ${processCount} -gt 1 ]; then
+		echo "$script is running"
+	else
+		#email me
+		echo "$script script not runing"
+		echo -e "$script stopped execution, attempting a restart" | mail -s "$script is not running" $email_address 
+		#`python $script &`
+	fi
+
+	#check disk usage
+	let p=`df -k $device | grep -v ^File | awk '{printf ("%i",$3*100 / $2); }'`
+	
+	if [ $p -ge 70 ]
+	then
+		df -h $device | mail -s "Harvester is low on space" $email_address
+	fi
+	
+	sleep 5
+done
