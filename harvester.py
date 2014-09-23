9134db05600b4080840b80db90491594c34782d3
Michael Akopov <makopov@gmail.com>
2014-09-23 16:45:22 -0600

    More attempts at doing cool things

--- harvester.py
+++ harvester.py
@@ -6,13 +6,19 @@ oTwitterAPI = twitter.Api(consumer_key='q1xeSRo0kXP7aNpRTd89UtKl7',
 	access_token_key='585462531-VsarINfKCH2eJTUGLgp6vCsVGoFzZZGPA9xWXGS3',
 	access_token_secret='Wvj4VXbZIJ3AYC95YepaM9Uxn4ya7n4klBl3X4tSo3d9i')
 
-statuses = oTwitterAPI.GetSearch(term='#nfl', count=3, result_type="recent", include_entities='true')
+statuses = oTwitterAPI.GetSearch(term='#broncos', count=500, result_type="recent", include_entities='true')
 
 for s in statuses:
-    print '%s\n' % s
+    print '%s\n' % s.text
+
+statuses = oTwitterAPI.GetSearch(term='#patriots', count=500, result_type="recent", include_entities='true')
+
+for s in statuses:
+    print '%s\n' % s.text
 
 
 iRateLimit = oTwitterAPI.GetRateLimitStatus()
-oJsonRateLimit = json.load(iRateLimit)
+#oJsonRateLimit = json.load(iRateLimit)
 
-print oJsonRateLimit["u'resources'"]
+print iRateLimit
+#print oJsonRateLimit
