f4a26a4193a672ed4e3ffbfadcefb5d4b160be4c
Michael Akopov <makopov@gmail.com>
2014-10-20 19:01:11 -0400

    keys are now read in via configuration file

--- harvester.py
+++ harvester.py
@@ -4,7 +4,7 @@ import time
 import os
 import sys
 import commands
-import datetime 
+import datetime
 #This is if we're running on none debian systems
 try:
     import apt
@@ -15,7 +15,7 @@ try:
         exit(1)
 except ImportError:
     print "\nWARNING:\tapt module not available to python, this script will be unable to determine if zip is available.\nPlease be sure the zip package is installed on this system, otherwise archiving will fail."
-    
+
     #Give the user a moment to read this message
     time.sleep(3)
 
@@ -24,12 +24,16 @@ class TweetHarvester:
     strConsumerSecret = ""
     strAccessTokenKey = ""
     strAccessTokenSecret = ""
-    
+    bUseGzip = True
+
     #Default constructor for setting up the object
     def __init__(self):
         self.readCommandLineArgs()
         self.loadConfiguration()
-        
+
+    def str2bool(self, bValue):
+      return bValue.lower() in ("yes", "true", "t", "1")
+
     def storeTweets(aTweets):
         bReturn = "true"
         oToday = datetime.date.today ()
@@ -52,17 +56,19 @@ class TweetHarvester:
         return bReturn
 
     def connectToTwitter(self):
-        return twitter.Api(consumer_key='rmkM62qY5Soqzp3hy40EnzZYS',
-    	consumer_secret='PfSsjuSurlSImFD5vTooiFGQlu0b6wkTj7WdiqupgRWlz3ydyK',
-    	access_token_key='585462531-2xVMADCejpNg2Z3TyqkLntYP1WkONDfa4CAhOe6X',
-    	access_token_secret='6sxch1mBdSw0cTB7lp40gBZgob5A7vjqGG2DxpZLmiJs2',
-        use_gzip_compression='true')
+        return twitter.Api(
+            consumer_key = self.strConsumerKey,
+            consumer_secret = self.strConsumerSecret,
+            access_token_key = self.strAccessTokenKey,
+            access_token_secret = self.strAccessTokenSecret,
+            use_gzip_compression = self.bUseGzip
+            )
 
     def archiveFiles(self, strFileName=''):
         #If no filename is passed in, lets set one
         if (strFileName == ''):
             #Lets determine the name of the last directory we created
-            oYesterday = datetime.date.today () - datetime.timedelta (days=1) 
+            oYesterday = datetime.date.today () - datetime.timedelta (days=1)
             strFileToArchive = str(oYesterday.strftime("%Y-%m-%d"))
 
             strFileName = strFileToArchive
@@ -73,7 +79,7 @@ class TweetHarvester:
 
         #Check if the file exists firest
         strFileExistsCommand = "ls " + strFileName
-        iFileExistsCommandStatus, strExistsCommandOutput = commands.getstatusoutput(strFileExistsCommand) 
+        iFileExistsCommandStatus, strExistsCommandOutput = commands.getstatusoutput(strFileExistsCommand)
 
         #If the file exists, lets continue, otherwise, lets exit
         if (iFileExistsCommandStatus == 0):
@@ -85,10 +91,10 @@ class TweetHarvester:
             else:
                 #If that succeeds lets remove the uncompressed version
                 print "Successfully archived '" + strFileName + "' to '" + strZipFileName + "'"
-            
+
                 strDeleteOldFileCommand = "rm -rf " + strFileName
                 iRmCommandStatus, strRmCommandOutput = commands.getstatusoutput(strDeleteOldFileCommand)
-            
+
                 if (iRmCommandStatus != 0):
                     print "Something went wrong while deleting the " + strFileName + " directory"
                     print "\tThe following command was issued: `" + strDeleteOldFileCommand + "`"
@@ -97,28 +103,24 @@ class TweetHarvester:
                     print "Successfully deleted uncompressed file '" + strFileName + "'"
         else:
             print "The file '" + strFileName + "' doesnt exist, nothing to compress"
-        
+
         #Put a sleep command in here to sleep this thread for 24 hours
         #print "Archiver sleeping for 24 hours"
         #time.sleep(86400)
 
-    def loadConfiguration(strConfigurationFile = ""):
+    def loadConfiguration(self, strConfigurationFile = ""):
         #If no filename is passed in, lets default to defaultConfig.conf
-        if not strConfigurationFile == "":
+        if strConfigurationFile == "":
             strConfigurationFile = 'defaultConfig.conf'
 
         oConfigFile = open(strConfigurationFile)
         oConfigData = json.load(oConfigFile)
 
-        for key, value in oConfigData.items():
-            switch(key) {
-                case "consumer_secret":
-                    print value
-                    #if value == ""
-                    break;
-                default:
-                    break;
-            }
+        self.strConsumerKey = oConfigData["consumer_key"]
+        self.strConsumerSecret = oConfigData["consumer_secret"]
+        self.strAccessTokenKey = oConfigData["access_token_key"]
+        self.strAccessTokenSecret = oConfigData["access_token_secret"]
+        self.bUseGzip = self.str2bool(oConfigData["use_gzip_compression"])
 
         oConfigFile.close()
 
@@ -128,7 +130,7 @@ class TweetHarvester:
 
     def run(self):
         oTwitterAPI = self.connectToTwitter()
-        
+
         #bContinue = True
         #
         #while bContinue:
@@ -197,7 +199,7 @@ class TweetHarvester:
         #
         #    #Lets see if theres anything to archive
         #    self.archiveFiles()
-        
+
         #    print "Sleeping for " + str(iTimeToSleepFor) + " seconds.."
         #    time.sleep(iTimeToSleepFor)
 
